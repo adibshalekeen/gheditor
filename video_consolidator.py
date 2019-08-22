7080d5bf8c9a2188c3625a51c3408969327fb633
Adib Shalekeen <ashaleke@edu.uwaterloo.ca>
2019-08-22 01:18:05 -0400

    working configurable sizes

--- video_consolidator.py
+++ video_consolidator.py
@@ -13,38 +13,40 @@ class ConsolidatedVideo:
         self.consolidate_video()
 
     def consolidate_video(self):
-        consolidating = True
-        frames = []
+        consolidating = True        
         while consolidating:
             output_frame = np.zeros(shape=(1080,1920,3))
-            for cap in self._video_captures:
-                ret , frame = cap.read()
+            for i in range(0, len(self._video_captures)):
+                ret , frame = self._video_captures[i].read()
                 consolidating = ret
                 if not ret:
                     break
-                frame = cv2.resize(frame, dsize=(int(1920/len(self._videos)), 1080))
-                frames.append(frame)
-            horizontal_slice = int(1920/len(self._videos))
-            for i in range(0, len(frames)):
-                output_frame[:, (i*horizontal_slice):((i+1)*horizontal_slice), :] = frames[i]
+                
+                slice_height = int(1080 / self._params["dims"][i][0])
+                slice_width = int(1920 / self._params["dims"][i][1])
+
+                frame = cv2.resize(frame, dsize=(slice_width, slice_height))
+
+                slice_offset = self._params["dims"][i][2] - 1
+
+                width = int(slice_width * slice_offset)
+                rows_travelled = 0
+                while width >= 1920:
+                    rows_travelled += 1
+                    width -= 1920
+                height = int(slice_height * rows_travelled)
+
+                output_frame[height : (height + slice_height), width : (width + slice_width), :] = frame
+
             output_frame = np.uint8(output_frame)
             self._video_writer.write(output_frame)
-            print(output_frame.shape)
-            cv2.imshow('frame', output_frame)
-            frames.clear()
+            cv2.imshow('Output', output_frame)
             cv2.waitKey(1)
+
         self._video_writer.release()
         for cap in self._video_captures:
             cap.release()
 
-    def extract_dims(self):
-        self._video_dimensions = {"fx": 0, "fy":0, "spans":[]}
-        min_x = 20
-        min_y = 20
-        for dim in self._params["dims"]:
-            if dim[0] < min_x:
-                min_x = dim[0]
-
 videos = ['./1.mp4', './2.mp4', './3.mp4']
-params = {"dims":[[2,2,1], [2,2,2], [2,1,1]]}
+params = {"dims":[[2,2,1], [2,2,2], [2,1,2]]}
 vid = ConsolidatedVideo(videos, params)
